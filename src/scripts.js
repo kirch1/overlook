3058ad19151769497782d177c2bedf574fe60017
Jason Kirchman <kirchman.jason@icloud.com>
2023-03-02 16:09:14 -0600

    Add new booking button and date picker

--- src/scripts.js
+++ src/scripts.js
@@ -1,14 +1,21 @@
+import { Datepicker } from 'vanillajs-datepicker';
 import { getData } from './api';
 import { User } from './classes/User';
 import './css/styles.css';
+import 'vanillajs-datepicker/css/datepicker-bulma.css';
 import './images/turing-logo.png';
-
 
 // QUERY SELECTORS
 const bookingsList = document.getElementById('bookings-list');
 const roomsTotal = document.getElementById('rooms-total'); 
 const userIcon = document.getElementById('user-icon'); 
 const userName = document.getElementById('user-name'); 
+const bookingsHeaderText = document.getElementById('bookings-header-text'); 
+const newBookingButton = document.getElementById('new-booking-button'); 
+const newBookingCancel = document.getElementById('new-booking-cancel'); 
+const newBookingToolbar = document.getElementById('new-booking-toolbar');
+const newBookingDate = document.getElementById('new-booking-date');
+const datePicker = new Datepicker(newBookingDate, {autohide: true, format: 'yyyy/mm/dd'}); 
 
 //DATA MODEL GLOBALS
 let user;
@@ -18,8 +25,24 @@ window.addEventListener('DOMContentLoaded', () => {
     getUserData();
 });
 
+newBookingButton.addEventListener('click', () => {
+    show(newBookingToolbar);
+    hide(newBookingButton);
+    show(newBookingCancel);
+});
+
+newBookingCancel.addEventListener('click', () => {
+    hide(newBookingToolbar);
+    show(newBookingButton);
+    hide(newBookingCancel);
+});
+
+newBookingDate.addEventListener('changeDate', (event) => {
+    console.log(newBookingDate.value)
+})
+
 const getUserData = () => {
-    Promise.all([getData('customers/1'), getData('bookings'), getData('rooms')])
+    Promise.all([getData('customers/12'), getData('bookings'), getData('rooms')])
         .then(data => {
             user = new User(data[0]);
             updateUserHeader();
@@ -32,6 +55,7 @@ const getUserData = () => {
 const updateUserHeader = () => {
     userIcon.innerText = user.name.split(' ')[0][0] + user.name.split(' ')[1][0];
     userName.innerText = user.name;
+    setBookingHeader(`${user.name.split(' ')[0]}'s Bookings`);
 }
 
 const updateBookingsList = () => {
@@ -47,11 +71,26 @@ const updateBookingsList = () => {
                         <label class="room-tag">bidet: ${booking.room.bidet ? 'yes' : 'no'}</label>
                     </div>
                 </div>
-                <label class="room-price">$${booking.room.costPerNight} / night</label>
+                <div class="room-info">
+                    <label class="room-info-text">${booking.date}</label>
+                    <label class="room-info-text">$${booking.room.costPerNight} / night</label>
+                </div>
             </section>`
     })
 }
 
 const updateRoomTotal = () => {
     roomsTotal.innerText = `$${user.getRoomTotal()}`
+}
+
+const setBookingHeader = (text) => {
+    bookingsHeaderText.innerHTML = text;
+}
+
+const show = (element) => {
+    element.classList.remove('hidden');
+}
+
+const hide = (element) => {
+    element.classList.add('hidden');
 }
